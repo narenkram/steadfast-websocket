06e2308272d354e82664a192a4f8ee83f335888c
Narendra Ram <narenramm@gmail.com>
2024-07-04 10:43:13 +0530

    remove unnecessary files

--- flattrade/f-websocket.py
+++ flattrade/f-websocket.py
@@ -1,38 +1,52 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-from flask_socketio import SocketIO, emit
 from api_helper import NorenApiPy
-import os
 from dotenv import load_dotenv
-
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-socketio = SocketIO(app, cors_allowed_origins="*")  # Enable WebSocket with CORS
+import os
+import logging
+import time
 
 # Load environment variables from .env file
 load_dotenv()
 
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Flag to tell us if the websocket is open
+socket_opened = False
+
+# Event handlers
+def event_handler_order_update(message):
+    print("order event: " + str(message))
+
+def event_handler_quote_update(message):
+    print("quote event: {0}".format(time.strftime('%d-%m-%Y %H:%M:%S')) + str(message))
+
+def open_callback():
+    global socket_opened
+    socket_opened = True
+    print('app is connected')
+    
+    # Subscribe to the desired symbol
+    api.subscribe(['NFO|55237'])
+
 # Initialize API
 api = NorenApiPy()
 
-@app.route('/get_limits', methods=['GET'])
-def get_limits():
-    usersession = os.getenv('USERSESSION')
-    userid = os.getenv('USERID')
-    api.set_session(userid=userid, password='', usertoken=usersession)
-    ret = api.get_limits()
-    return jsonify(ret)
+# Set token and user id
+usersession = os.getenv('USERSESSION')
+userid = os.getenv('USERID')
 
-@socketio.on('subscribe')
-def handle_subscribe(data):
-    symbol = data.get('symbol')
-    if symbol:
-        # Example subscription logic
-        api.subscribe(symbol, on_price_update)
+# Set up the session
+ret = api.set_session(userid=userid, password='', usertoken=usersession)
 
-def on_price_update(price_data):
-    # Emit the price update to the client
-    socketio.emit('price_update', price_data)
+if ret is not None:
+    # Start the websocket
+    ret = api.start_websocket(
+        order_update_callback=event_handler_order_update,
+        subscribe_callback=event_handler_quote_update,
+        socket_open_callback=open_callback
+    )
+    print(ret)
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+# Keep the script running
+while True:
+    time.sleep(1)
