dffbe435390ad23f0513b6edd254d5fdb3bd3db2
Narendra Ram <narenramm@gmail.com>
2024-10-21 12:00:03 +0530

    add env config

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
 import os
 
 # Determine the environment
-ENV = os.getenv("ENV", "production")
+ENV = os.getenv("ENV", "development")
 
 if ENV == "production":
     # Production settings
     BASE_URL = "https://api.steadfastapp.in"
-else:
+    FLATTRADE_WS_PORT = int(os.getenv("FLATTRADE_WS_PORT", 8765))
+    SHOONYA_WS_PORT = int(os.getenv("SHOONYA_WS_PORT", 8766))
+elif ENV == "development":
     # Development settings
     BASE_URL = "http://localhost:3000"
+    FLATTRADE_WS_PORT = int(os.getenv("FLATTRADE_WS_PORT", 8765))
+    SHOONYA_WS_PORT = int(os.getenv("SHOONYA_WS_PORT", 8766))
+else:
+    raise ValueError(f"Invalid environment: {ENV}")
 
 # API Endpoints
 FLATTRADE_WEBSOCKET_DATA_ENDPOINT = f"{BASE_URL}/flattrade/websocketData"
@@ -16,5 +22,3 @@ SHOONYA_WEBSOCKET_DATA_ENDPOINT = f"{BASE_URL}/shoonya/websocketData"
 
 # WebSocket server settings
 WS_HOST = "0.0.0.0"  # Listen on all available interfaces
-FLATTRADE_WS_PORT = 8765
-SHOONYA_WS_PORT = 8766
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
+from dotenv import load_dotenv
 from flattrade.flattrade_websocket import main as flattrade_main
 from shoonya.shoonya_websocket import main as shoonya_main
 
+# Load environment variables from .env file
+load_dotenv()
 
 async def main():
     await asyncio.gather(flattrade_main(), shoonya_main())
-
 
 if __name__ == "__main__":
     asyncio.run(main())
